f2fabe2bb93e85efb77e1c9cfd1e8fe6aaed80a1
David <daclarke@outlook.com>
2013-07-03 09:16:08 -0700

    removing old code

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,14 +1,28 @@
 //2-Way motor control
 
-int motorPin1 =  5;    // One motor wire connected to digital pin 5
-int motorPin2 =  6;    // One motor wire connected to digital pin 6
+//Motor 1
+int motorPin1 =  5;    
+int motorPin2 =  6;    
+
+//Motor 2
 int motorPin3 =  10;
 int motorPin4 =  11;
+
+//Motor 3
+int motorPin5 =  3;
+int motorPin6 =  9;
 
 int incomingByte = 0; 
 
 int leftMotor = 0;
 int rightMotor = 0;
+
+boolean forward = false;
+boolean reverse = false;
+boolean right = false;
+boolean left = false;
+boolean up = false;
+boolean down = false;
 
 // The setup() method runs once, when the sketch starts
 void setup()   {                
@@ -19,79 +33,127 @@ void setup()   {
   pinMode(motorPin3, OUTPUT); 
   pinMode(motorPin4, OUTPUT);  
   
-  Serial.begin(115200);
+  Serial.begin(9600);
 }
 
 // the loop() method runs over and over again,
 // as long as the Arduino has power
 void loop()                     
 {
+  digitalWrite(motorPin4, HIGH);
+  digitalWrite(motorPin3, LOW);
     
   if (Serial.available() > 0) {
     incomingByte = Serial.read();
     
-    Serial.write(incomingByte);
-    
-    if(incomingByte == 70){ //F
-        leftMotor += 150;
-        rightMotor += 150;
+    if(incomingByte == 70){         //F    
+        digitalWrite(motorPin2, HIGH);
+        digitalWrite(motorPin1, LOW);
+        //forward = true;
     } else if(incomingByte == 102){ //f
-        leftMotor -= 150;
-        rightMotor -= 150;
-    } else if(incomingByte == 82){ //R      
-        leftMotor += 100;
-        rightMotor -= 100;
-    } else if(incomingByte == 114){ //r    
-        leftMotor -= 100;
-        rightMotor += 100;
-    } else if(incomingByte == 76){ //L     
-        leftMotor -= 100;
-        rightMotor += 100;
-    } else if(incomingByte == 108){ //l   
-        leftMotor += 100;
-        rightMotor -= 100;
-    } else if(incomingByte == 86){ //V  
-        leftMotor -= 150;
-        rightMotor -= 150;
+        forward = false;
+    }
+    if(incomingByte == 86){         //V  
+        reverse = true;
     } else if(incomingByte == 118){ //v     
-        leftMotor += 150;
-        rightMotor += 150;
+        reverse = false;
+    } 
+    
+    if(incomingByte == 82){         //R      
+        right = true;
+    } else if(incomingByte == 114){ //r    
+        right = false;
+    }
+    if(incomingByte == 76){         //L     
+        left = true;
+    } else if(incomingByte == 108){ //l   
+        left = false;
+    } else 
+    
+    if(incomingByte == 85){         //U  
+        up = true;
+    } else if(incomingByte == 117){ //u     
+        up = false;
+    }
+    if(incomingByte == 68){         //D  
+        down = true;
+    } else if(incomingByte == 100){ //d     
+        down = false;
     }
   }
+  delay(10);
   
-  if(rightMotor > 255){
-    rightMotor = 250;
-  } else if(rightMotor < -255) {
-    rightMotor = -250;
-  }
-  if(leftMotor > 255) {
-    leftMotor = 250;
-  } else if(leftMotor < -255) {
-    leftMotor = -250;
+  if(forward && right && !left){
+    move_right(255);
+  } else if(forward && !right && left){
+    move_left(255);
+  } else if(forward){
+    move_forward(255);
   }
   
-  delay(100);
+  if(reverse && right && !left){
+    move_right(255);
+  } else if(reverse && !right && left){
+    move_left(255);
+  } else if(reverse){
+    move_reverse(255);
+  }  
+  
+  if(up){
+    move_up(255);
+  } else if(down){
+    move_down(255);
+  }
+  
   rotateLeftMotor(leftMotor);
   rotateRightMotor(rightMotor);
 }
 
+void move_forward(int motorSpeed){
+  rotateLeftMotor(motorSpeed);
+  rotateRightMotor(motorSpeed);
+}
+
+void move_reverse(int motorSpeed){
+  rotateLeftMotor(-motorSpeed);
+  rotateRightMotor(-motorSpeed);
+}
+
+void move_left(int motorSpeed){
+  rotateLeftMotor(-motorSpeed);
+  rotateRightMotor(motorSpeed);
+}
+
+void move_right(int motorSpeed){
+  rotateLeftMotor(motorSpeed);
+  rotateRightMotor(-motorSpeed);
+}
+
+void move_up(int motorSpeed){
+  
+}
+
+void move_down(int motorSpeed){
+  
+}
+
 void rotateLeftMotor(int leftMotorSpeed){  
-  if(leftMotorSpeed >= 0 && leftMotorSpeed < 255){
+  if(leftMotorSpeed >= 0 && leftMotorSpeed <= 255){
     analogWrite(motorPin2, leftMotorSpeed);
     digitalWrite(motorPin1, LOW);
   }
-  else if(leftMotorSpeed < 0 && leftMotorSpeed > -255){
+  else if(leftMotorSpeed < 0 && leftMotorSpeed >= -255){
     analogWrite(motorPin1, -leftMotorSpeed);
     digitalWrite(motorPin2, LOW);
   }
 }
 
 void rotateRightMotor(int rightMotorSpeed){  
-  if(rightMotorSpeed >= 0 && rightMotorSpeed < 255){
+  if(rightMotorSpeed >= 0 && rightMotorSpeed <= 255){
     analogWrite(motorPin4, rightMotorSpeed);
     digitalWrite(motorPin3, LOW);
   }
-  else if(rightMotorSpeed < 0 && rightMotorSpeed > -255){
+  else if(rightMotorSpeed < 0 && rightMotorSpeed >= -255){
     analogWrite(motorPin3, -rightMotorSpeed);
     digitalWrite(motorPin4, LOW);
   }
--- src/com/example/arduinocontroller/MainActivity.java
+++ src/com/example/arduinocontroller/MainActivity.java
@@ -39,7 +39,6 @@ public class MainActivity extends Activity {
     private OutputStream outStream = null;
 	public String address = "00:07:80:50:78:BE";
     private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-    private InputStream inStream = null;
 	Handler handler = new Handler();
 	byte delimiter = 10;
 	boolean stopWorker = false;
@@ -66,16 +65,16 @@ public class MainActivity extends Activity {
 	
 	//Arduino command codes
 	private static Byte cForward = 70;		//F
-	private static Byte cStopForward = 102;//f
+	private static Byte cStopForward = 102;	//f
 	private static Byte cRight = 82;		//R
 	private static Byte cStopRight = 114;	//r
-	private static Byte cLeft = 76;		//L
+	private static Byte cLeft = 76;			//L
 	private static Byte cStopLeft = 108;	//l
 	private static Byte cReverse = 86;		//V
-	private static Byte cStopReverse = 118;//v
+	private static Byte cStopReverse = 118;	//v
 	private static Byte cUp = 85;			//U
 	private static Byte cStopUp = 117;		//u
-	private static Byte cDown = 68;		//D
+	private static Byte cDown = 68;			//D
 	private static Byte cStopDown = 100;	//d
 	
 	Map<Byte,Boolean> outputCommands = new HashMap<Byte,Boolean>();
@@ -92,7 +91,8 @@ public class MainActivity extends Activity {
         Connect.setOnClickListener(new OnClickListener() {			
 			@Override
 			public void onClick(View v) {
-				//Connect();		
+				Connect();				        
+		        beginSendCommandsThread();
 				SetUpPlayerOptionMenu();
 			}
 		});
@@ -136,8 +136,6 @@ public class MainActivity extends Activity {
 			}
 			Log.d("", "Socket creation failed");
 		}
-		
-		beginSendCommandsThread();
     }
 	
 	private void writeData(byte data) {
@@ -177,16 +175,14 @@ public class MainActivity extends Activity {
 				SetUpOnePlayer();
 			}
     	});
-
-    	mTwoPlayerButton = (Button) findViewById(R.id.button_num_players_one);
+    	mTwoPlayerButton = (Button) findViewById(R.id.button_num_players_two);
     	mTwoPlayerButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
 				SetUpTwoPlayer();
 			}
     	});
-
-    	mThreePlayerButton = (Button) findViewById(R.id.button_num_players_one);
+    	mThreePlayerButton = (Button) findViewById(R.id.button_num_players_three);
     	mThreePlayerButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
@@ -257,6 +253,20 @@ public class MainActivity extends Activity {
 	public void SetUpOnePlayer() {
 		setContentView(R.layout.one_player);
 
+		resetOutputMap();
+		outputCommands.put(cForward,false);
+		outputCommands.put(cStopForward,true);
+		outputCommands.put(cRight,false);
+		outputCommands.put(cStopRight,true);
+		outputCommands.put(cLeft,false);
+		outputCommands.put(cStopLeft,true);
+		outputCommands.put(cReverse,false);
+		outputCommands.put(cStopReverse,true);
+		outputCommands.put(cUp,false);
+		outputCommands.put(cStopUp,true);
+		outputCommands.put(cDown,false);
+		outputCommands.put(cStopDown,true);
+		
 		mUpButton = (Button) findViewById(R.id.button_vertical_up);
 		mUpButton.setOnTouchListener(new OnTouchListener() {
 		     @Override
@@ -270,8 +280,7 @@ public class MainActivity extends Activity {
 		        }
 				return false;
 		     }
-		});
-		
+		});		
 		mDownButton = (Button) findViewById(R.id.button_vertical_down);
 		mDownButton.setOnTouchListener(new OnTouchListener() {
 		     @Override
@@ -285,7 +294,131 @@ public class MainActivity extends Activity {
 		        }
 				return false;
 		     }
+		});		
+		mForwardButton = (Button) findViewById(R.id.button_up);
+		mForwardButton.setOnTouchListener(new OnTouchListener() {
+		     @Override
+		     public boolean onTouch(View v, MotionEvent event) {
+		        if(event.getAction() == MotionEvent.ACTION_DOWN) { 
+		        	outputCommands.put(cForward, true);
+		        	outputCommands.put(cStopForward, false);
+		        } else if (event.getAction() == MotionEvent.ACTION_UP) { 
+		        	outputCommands.put(cForward, true);
+		        	outputCommands.put(cStopForward, false);
+		        }
+				return false;
+		     }
 		});
+		mReverseButton = (Button) findViewById(R.id.button_down);
+		mReverseButton.setOnTouchListener(new OnTouchListener() {
+		     @Override
+		     public boolean onTouch(View v, MotionEvent event) {
+		        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+		        	outputCommands.put(cReverse, true);
+		        	outputCommands.put(cStopReverse, false);
+		        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+		        	outputCommands.put(cStopReverse, true);
+		        	outputCommands.put(cReverse, false);
+		        }
+				return false;
+		     }
+		});		
+		mLeftButton = (Button) findViewById(R.id.button_left);
+		mLeftButton.setOnTouchListener(new OnTouchListener() {
+		     @Override
+		     public boolean onTouch(View v, MotionEvent event) {
+		        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+		        	outputCommands.put(cLeft, true);
+		        	outputCommands.put(cStopLeft, false);
+		        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+		        	outputCommands.put(cStopLeft, true);
+		        	outputCommands.put(cLeft, false);
+		        }
+				return false;
+		     }
+		});		
+		mRightButton = (Button) findViewById(R.id.button_right);
+		mRightButton.setOnTouchListener(new OnTouchListener() {
+		     @Override
+		     public boolean onTouch(View v, MotionEvent event) {
+		        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+		        	outputCommands.put(cRight, true);
+		        	outputCommands.put(cStopRight, false);
+		        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+		        	outputCommands.put(cStopRight, true);
+		        	outputCommands.put(cRight, false);
+		        }
+				return false;
+		     }
+		});		
+		mBackMenuButton = (Button) findViewById(R.id.button_back_player_menu);
+		mBackMenuButton.setOnClickListener(new OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				SetUpPlayerOptionMenu();
+			}
+		});
+	}
+	
+
+    protected void SetUpTwoPlayer_PlayerTwo() {
+		setContentView(R.layout.twoplayer_playertwo);
+
+		resetOutputMap();
+		outputCommands.put(cUp,false);
+		outputCommands.put(cStopUp,true);
+		outputCommands.put(cDown,false);
+		outputCommands.put(cStopDown,true);
+		
+		mUpButton = (Button) findViewById(R.id.button_vertical_up);
+		mUpButton.setOnTouchListener(new OnTouchListener() {
+		     @Override
+		     public boolean onTouch(View v, MotionEvent event) {
+		        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+		        	outputCommands.put(cUp, true);
+		        	outputCommands.put(cStopUp, false);
+		        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+		        	outputCommands.put(cStopUp, true);
+		        	outputCommands.put(cUp, false);
+		        }
+				return false;
+		     }
+		});		
+		mDownButton = (Button) findViewById(R.id.button_vertical_down);
+		mDownButton.setOnTouchListener(new OnTouchListener() {
+		     @Override
+		     public boolean onTouch(View v, MotionEvent event) {
+		        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+		        	outputCommands.put(cDown, true);
+		        	outputCommands.put(cStopDown, false);
+		        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+		        	outputCommands.put(cStopDown, true);
+		        	outputCommands.put(cDown, false);
+		        }
+				return false;
+		     }
+		});		
+		mBackMenuButton = (Button) findViewById(R.id.button_back_player_menu);
+		mBackMenuButton.setOnClickListener(new OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				SetUpTwoPlayer();
+			}
+		});
+	}
+
+	protected void SetUpTwoPlayer_PlayerOne() {
+		setContentView(R.layout.twoplayer_playerone);
+
+		resetOutputMap();
+		outputCommands.put(cForward,false);
+		outputCommands.put(cStopForward,true);
+		outputCommands.put(cRight,false);
+		outputCommands.put(cStopRight,true);
+		outputCommands.put(cLeft,false);
+		outputCommands.put(cStopLeft,true);
+		outputCommands.put(cReverse,false);
+		outputCommands.put(cStopReverse,true);
 		
 		mForwardButton = (Button) findViewById(R.id.button_up);
 		mForwardButton.setOnTouchListener(new OnTouchListener() {
@@ -301,7 +434,6 @@ public class MainActivity extends Activity {
 				return false;
 		     }
 		});
-
 		mReverseButton = (Button) findViewById(R.id.button_down);
 		mReverseButton.setOnTouchListener(new OnTouchListener() {
 		     @Override
@@ -315,8 +447,7 @@ public class MainActivity extends Activity {
 		        }
 				return false;
 		     }
-		});
-		
+		});		
 		mLeftButton = (Button) findViewById(R.id.button_left);
 		mLeftButton.setOnTouchListener(new OnTouchListener() {
 		     @Override
@@ -330,8 +461,7 @@ public class MainActivity extends Activity {
 		        }
 				return false;
 		     }
-		});
-		
+		});		
 		mRightButton = (Button) findViewById(R.id.button_right);
 		mRightButton.setOnTouchListener(new OnTouchListener() {
 		     @Override
@@ -346,32 +476,6 @@ public class MainActivity extends Activity {
 				return false;
 		     }
 		});
-		
-		mBackMenuButton = (Button) findViewById(R.id.button_back_player_menu);
-		mBackMenuButton.setOnClickListener(new OnClickListener() {			
-			@Override
-			public void onClick(View v) {
-				SetUpPlayerOptionMenu();
-			}
-		});
-	}
-	
-
-    protected void SetUpTwoPlayer_PlayerTwo() {
-		// TODO Auto-generated method stub
-
-		mBackMenuButton = (Button) findViewById(R.id.button_back_player_menu);
-		mBackMenuButton.setOnClickListener(new OnClickListener() {			
-			@Override
-			public void onClick(View v) {
-				SetUpTwoPlayer();
-			}
-		});
-	}
-
-	protected void SetUpTwoPlayer_PlayerOne() {
-		// TODO Auto-generated method stub
-
 		mBackMenuButton = (Button) findViewById(R.id.button_back_player_menu);
 		mBackMenuButton.setOnClickListener(new OnClickListener() {			
 			@Override
@@ -383,8 +487,42 @@ public class MainActivity extends Activity {
 
     
 	protected void SetUpThreePlayer_PlayerThree() {
-		// TODO Auto-generated method stub
+		setContentView(R.layout.threeplayer_playerthree);
 
+		resetOutputMap();
+		outputCommands.put(cUp,false);
+		outputCommands.put(cStopUp,true);
+		outputCommands.put(cDown,false);
+		outputCommands.put(cStopDown,true);
+		
+		mUpButton = (Button) findViewById(R.id.button_vertical_up);
+		mUpButton.setOnTouchListener(new OnTouchListener() {
+		     @Override
+		     public boolean onTouch(View v, MotionEvent event) {
+		        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+		        	outputCommands.put(cUp, true);
+		        	outputCommands.put(cStopUp, false);
+		        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+		        	outputCommands.put(cStopUp, true);
+		        	outputCommands.put(cUp, false);
+		        }
+				return false;
+		     }
+		});		
+		mDownButton = (Button) findViewById(R.id.button_vertical_down);
+		mDownButton.setOnTouchListener(new OnTouchListener() {
+		     @Override
+		     public boolean onTouch(View v, MotionEvent event) {
+		        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+		        	outputCommands.put(cDown, true);
+		        	outputCommands.put(cStopDown, false);
+		        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+		        	outputCommands.put(cStopDown, true);
+		        	outputCommands.put(cDown, false);
+		        }
+				return false;
+		     }
+		});	
 		mBackMenuButton = (Button) findViewById(R.id.button_back_player_menu);
 		mBackMenuButton.setOnClickListener(new OnClickListener() {			
 			@Override
@@ -395,8 +533,42 @@ public class MainActivity extends Activity {
 	}
 
 	protected void SetUpThreePlayer_PlayerTwo() {
-		// TODO Auto-generated method stub
+		setContentView(R.layout.threeplayer_playertwo);
 
+		resetOutputMap();
+		outputCommands.put(cRight,false);
+		outputCommands.put(cStopRight,true);
+		outputCommands.put(cLeft,false);
+		outputCommands.put(cStopLeft,true);
+		
+		mLeftButton = (Button) findViewById(R.id.button_left);
+		mLeftButton.setOnTouchListener(new OnTouchListener() {
+		     @Override
+		     public boolean onTouch(View v, MotionEvent event) {
+		        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+		        	outputCommands.put(cLeft, true);
+		        	outputCommands.put(cStopLeft, false);
+		        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+		        	outputCommands.put(cStopLeft, true);
+		        	outputCommands.put(cLeft, false);
+		        }
+				return false;
+		     }
+		});		
+		mRightButton = (Button) findViewById(R.id.button_right);
+		mRightButton.setOnTouchListener(new OnTouchListener() {
+		     @Override
+		     public boolean onTouch(View v, MotionEvent event) {
+		        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+		        	outputCommands.put(cRight, true);
+		        	outputCommands.put(cStopRight, false);
+		        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+		        	outputCommands.put(cStopRight, true);
+		        	outputCommands.put(cRight, false);
+		        }
+				return false;
+		     }
+		});
 		mBackMenuButton = (Button) findViewById(R.id.button_back_player_menu);
 		mBackMenuButton.setOnClickListener(new OnClickListener() {			
 			@Override
@@ -407,8 +579,42 @@ public class MainActivity extends Activity {
 	}
 
 	protected void SetUpThreePlayer_PlayerOne() {
-		// TODO Auto-generated method stub
-
+		setContentView(R.layout.threeplayer_playerone);
+		
+		resetOutputMap();
+		outputCommands.put(cForward,false);
+		outputCommands.put(cStopForward,true);
+		outputCommands.put(cReverse,false);
+		outputCommands.put(cStopReverse,true);
+		
+		mForwardButton = (Button) findViewById(R.id.button_up);
+		mForwardButton.setOnTouchListener(new OnTouchListener() {
+		     @Override
+		     public boolean onTouch(View v, MotionEvent event) {
+		        if(event.getAction() == MotionEvent.ACTION_DOWN) { 
+		        	outputCommands.put(cForward, true);
+		        	outputCommands.put(cStopForward, false);
+		        } else if (event.getAction() == MotionEvent.ACTION_UP) { 
+		        	outputCommands.put(cForward, true);
+		        	outputCommands.put(cStopForward, false);
+		        }
+				return false;
+		     }
+		});
+		mReverseButton = (Button) findViewById(R.id.button_down);
+		mReverseButton.setOnTouchListener(new OnTouchListener() {
+		     @Override
+		     public boolean onTouch(View v, MotionEvent event) {
+		        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+		        	outputCommands.put(cReverse, true);
+		        	outputCommands.put(cStopReverse, false);
+		        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+		        	outputCommands.put(cStopReverse, true);
+		        	outputCommands.put(cReverse, false);
+		        }
+				return false;
+		     }
+		});
 		mBackMenuButton = (Button) findViewById(R.id.button_back_player_menu);
 		mBackMenuButton.setOnClickListener(new OnClickListener() {			
 			@Override
@@ -417,69 +623,61 @@ public class MainActivity extends Activity {
 			}
 		});
 	}
-
 	
+	private void resetOutputMap() {
+		outputCommands = null;
+		outputCommands = new HashMap<Byte,Boolean>();
+	}
+
 	//This thread will loop ~10 times a second. Will output 6 commands per loop, 1 for each direction.
 	
-	//TODO: remove keys from outputCommands depending on the player. Not all players need to send all commands.
 	public void beginSendCommandsThread(){
-		
-		outputCommands.put(cForward,false);
-		outputCommands.put(cStopForward,true);
-		outputCommands.put(cRight,false);
-		outputCommands.put(cStopRight,true);
-		outputCommands.put(cLeft,false);
-		outputCommands.put(cStopLeft,true);
-		outputCommands.put(cReverse,false);
-		outputCommands.put(cStopReverse,true);
-		outputCommands.put(cUp,false);
-		outputCommands.put(cStopUp,true);
-		outputCommands.put(cDown,false);
-		outputCommands.put(cStopDown,true);
 		
 		Thread commandThread = new Thread(new Runnable(){
 			@Override
 			public void run() {
-				if(outputCommands.get(cForward)){
-					writeData(cForward);
-				}
-				if(outputCommands.get(cStopForward)){
-					writeData(cStopForward);
-				}
-				if(outputCommands.get(cRight)){
-					writeData(cRight);
-				}
-				if(outputCommands.get(cStopRight)){
-					writeData(cStopRight);
-				}
-				if(outputCommands.get(cLeft)){
-					writeData(cLeft);
-				}
-				if(outputCommands.get(cStopLeft)){
-					writeData(cStopLeft);
-				}
-				if(outputCommands.get(cReverse)){
-					writeData(cReverse);
-				}
-				if(outputCommands.get(cStopReverse)){
-					writeData(cStopReverse);
-				}
-				if(outputCommands.get(cUp)){
-					writeData(cUp);
-				}
-				if(outputCommands.get(cStopUp)){
-					writeData(cStopUp);
-				}
-				if(outputCommands.get(cDown)){
-					writeData(cDown);
-				}
-				if(outputCommands.get(cStopDown)){
-					writeData(cStopDown);
-				}
-				try {
-					Thread.sleep(100);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
+				for(;;){
+					if(outputCommands.get(cForward) != null && outputCommands.get(cForward) == true){
+						writeData(cForward);
+					}
+					if(outputCommands.get(cStopForward) != null && outputCommands.get(cStopForward) == true){
+						writeData(cStopForward);
+					}
+					if(outputCommands.get(cRight) != null && outputCommands.get(cRight) == true){
+						writeData(cRight);
+					}
+					if(outputCommands.get(cStopRight) != null && outputCommands.get(cStopRight) == true){
+						writeData(cStopRight);
+					}
+					if(outputCommands.get(cLeft) != null && outputCommands.get(cLeft) == true){
+						writeData(cLeft);
+					}
+					if(outputCommands.get(cStopLeft) != null && outputCommands.get(cStopLeft) == true){
+						writeData(cStopLeft);
+					}
+					if(outputCommands.get(cReverse) != null && outputCommands.get(cReverse) == true){
+						writeData(cReverse);
+					}
+					if(outputCommands.get(cStopReverse) != null && outputCommands.get(cStopReverse) == true){
+						writeData(cStopReverse);
+					}
+					if(outputCommands.get(cUp) != null && outputCommands.get(cUp) == true){
+						writeData(cUp);
+					}
+					if(outputCommands.get(cStopUp) != null && outputCommands.get(cStopUp) == true){
+						writeData(cStopUp);
+					}
+					if(outputCommands.get(cDown) != null && outputCommands.get(cDown) == true){
+						writeData(cDown);
+					}
+					if(outputCommands.get(cStopDown) != null && outputCommands.get(cStopDown) == true){
+						writeData(cStopDown);
+					}
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
 				}
 			}			
 		});
